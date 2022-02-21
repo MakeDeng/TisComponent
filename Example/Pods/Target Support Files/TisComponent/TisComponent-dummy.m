6f6ae11afd0e320b88c1ac9ae2f383ac675a8479
dengchaoyun <dengchaoyun@zhaoshang.net>
2022-02-21 11:28:50 +0800

    框架搭建

---
+++ Example/Pods/Target Support Files/TisComponent/TisComponent-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TisComponent : NSObject
+@end
+@implementation PodsDummy_TisComponent
+@end
---
+++ TisComponent/Classes/TISLabel.m
@@ -0,0 +1,21 @@
+//
+//  TISLabel.m
+//  TisComponent_Example
+//
+//  Created by tanikawa on 2022/2/21.
+//  Copyright © 2022 dengchaoyun. All rights reserved.
+//
+
+#import "TISLabel.h"
+
+@implementation TISLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
